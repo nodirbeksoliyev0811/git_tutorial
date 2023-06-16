a69cf012d7a4d7b03089a37df9273d5feb7cd7a6
Nodirchik <nodirchiksoliyev.@gmail.com>
2023-06-16 14:31:23 +0500

    part 1

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,9 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true
       ),
-      home: const WelcomeScreen(),
+      home: Column(
+        children: [],
+      ),
     );
   }
 }
