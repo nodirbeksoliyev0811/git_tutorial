6e297f1bd15dd965dffc4276877bc378b792571d
Nodirbek Soliyev <116708762+nodirbeksoliyev0811@users.noreply.github.com>
2023-06-16 20:06:04 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -16,14 +16,10 @@ class MyApp extends StatelessWidget {
       ),
       home: Column(
         children: [
-          Text('Commit qiliw ucn'),
+          Text("Nodirchik",style: TextStyle(color: Colors.red,fontSize: 23,fontWeight: FontWeight.w900),),
           Text("Jamshid Jo'rayev",style: TextStyle(fontWeight: FontWeight.w700,fontSize: 24,color: Colors.black),),
           Text('Muhammad'),
-          Text("Nodirchik",style: TextStyle(color: Colors.red,fontSize: 23,fontWeight: FontWeight.w900),),
-          Text("Dilshodbek", style: TextStyle(
-            fontSize: 35,
-            color: Colors.tealAccent
-          ),)
+          Text("Dilshodbek", style: TextStyle(fontSize: 35,color: Colors.tealAccent),),
         ],
       ),
     );
